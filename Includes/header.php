c0d3307b290ff9661965502ffaa0e5f0aafdbacb
Unknown <atonysanchez1@gmail.com>
2017-07-10 16:02:13 -0700

    First Commit
    Home page template is set up along with insert footer and header.

---
+++ Includes/header.php
@@ -0,0 +1,25 @@
+<?php
+echo'
+
+<div class = "container-fluid m-0 p-0 csulb">
+  <a href ="http://www.csulb.edu/"class = "lead text-white">California State University, Long Beach </a>
+</div>
+<nav class="navbar navbar-toggleable-sm navbar-light">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#"><img src="../Images/pyrmid.jpg" width="90" height="80" class="d-inline-block align-top" alt = ""></a>
+  <div class="collapse navbar-collapse text-center justify-content-end" id="navbarSupportedContent">
+        <a class="nav-item nav-link h4 active" href="beachcavehome.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link h4" href="research.php">Research</a>
+        <a class="nav-item nav-link h4" href="lab.php">Lab</a>
+        <a class="nav-item nav-link h4" href="faculty.php">Faculty</a>
+        <a class="nav-item nav-link h4" href="news.php">News & Events</a>
+      <!--<form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-outline-primary my-2 my-sm-0 p-0" type="submit">
+      </button>
+    </form>-->
+  </div>
+</nav>';
+?>
---
+++ beachcavepages/beachcavehome.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Icons Link -->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="../CSS/Style-Color.css">
+  </head>
+  <body>
+    <!--Including the header-->
+    <?php include '../Includes/header.php'; ?>
+    <div class = "jumbotron jumbotron-fluid text-white text-center m-0 bgCol" >
+        <div class = "container">
+          <h1 class = "display-1">Welcome to the <strong>Beach</strong>Cave</h1>
+          <p class = "lead">An immersive virtual reality environment that will facilitate ongoing interdisciplinary research at California State University, Long Beach</p>
+        </div>
+      </div>
+      <div class = "container-fluid  m-0 p-0">
+        <div class = "row m-0 p-0" >
+          <div class = "col-md-12 col-lg-4 col-sm-12 p-0 m-0 ">
+            <div class = "card text-center border-0 rounded-0">
+              <img class = "card-img rounded-0" src="../Images/HRInteraction.jpg" alt = "Card image">
+              <div class = "card-img-overlay">
+                <div class  ="row justify-content-center" style = "height:80%">
+                  <h4 class = "card-title text-white">Human-robot Interaction</h4>
+                </div>
+                <div class = "row justify-content-center" >
+                  <a href="#" class=" btn btn-outline-primary">Find out more</a>
+                </div>
+              </div>
+          </div>
+        </div>
+        <div class = "col-md-12 col-lg-4 col-sm-12 p-0 m-0">
+          <div class = "card text-center border-0 rounded-0">
+            <img class = "card-img  rounded-0" src="../Images/ComplexSystems.jpg" alt = "Card image">
+            <div class = "card-img-overlay">
+              <div class = "row justify-content-center" style = "height:80%">
+                <h4 class = "card-title text-white">Human interaction in complex systems</h4>
+              </div>
+              <div class = "row justify-content-center"  >
+                <a href="#" class="btn btn-outline-primary">Find out more</a>
+              </div>
+            </div>
+        </div>
+      </div>
+      <div class = "col-md-12 col-lg-4 col-sm-12 p-0 m-0">
+        <div class = "card text-center border-0 rounded-0">
+          <img class = "card-img rounded-0" src="../Images/HumanPerformance.jpg" alt = "Card image">
+          <div class = "card-img-overlay">
+            <div class = "row justify-content-center" style = "height:80%">
+              <h4 class = "card-title text-white">Human motor and learning performance</h4>
+            </div>
+            <div class = "row justify-content-center">
+              <a href="#" class="btn btn-outline-primary">Find out more</a>
+            </div>
+          </div>
+      </div>
+    </div>
+      </div>
+          <!--Including Footer-->
+          <?php include '../Includes/footer.php';?>
+
+    <!--</div>
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+  </body>
+</html>
